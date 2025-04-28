0e1becd31fd64dd3c207ca5e8cbfc354baca3dff
João Vitor Maia <maia.tostring@gmail.com>
2025-04-28 01:28:34 -0300

    fix: i'm tired boss

--- Integration/WhatsAppEvolutionIntegration.php
+++ Integration/WhatsAppEvolutionIntegration.php
@@ -3,7 +3,7 @@
 namespace MauticPlugin\MauticWhatsAppEvolutionBundle\Integration;
 
 use Mautic\IntegrationsBundle\Integration\BasicIntegration;
-use Mautic\IntegrationsBundle\Integration\ConfigurationTrait;
+use Mautic\IntegrationsBundle\Integration\DefaultConfigFormTrait;
 use Mautic\IntegrationsBundle\Integration\Interfaces\BasicInterface;
 use Mautic\IntegrationsBundle\Integration\Interfaces\ConfigFormInterface;
 use Mautic\IntegrationsBundle\Integration\Interfaces\ConfigFormAuthInterface;
@@ -11,9 +11,10 @@ use MauticPlugin\MauticWhatsAppEvolutionBundle\Form\Type\ConfigType;
 
 class WhatsAppEvolutionIntegration extends BasicIntegration implements BasicInterface, ConfigFormInterface, ConfigFormAuthInterface
 {
-    use ConfigurationTrait;
+    use DefaultConfigFormTrait;
 
     public const NAME = 'WhatsAppEvolution';
+
     public const DISPLAY_NAME = 'WhatsApp Evolution API';
 
     public function getName(): string
@@ -31,20 +32,8 @@ class WhatsAppEvolutionIntegration extends BasicIntegration implements BasicInte
         return 'plugins/MauticWhatsAppEvolutionBundle/Assets/img/evolution-logo.png';
     }
 
-    public function getConfigFormName(): string | null
-    {
-        return null;
-        return ConfigType::class;
-    }
-
     public function getAuthConfigFormName(): string
     {
         return ConfigType::class;
-    }
-
-    public function getConfigFormContentTemplate(): string | null
-    {
-        return null;
-        return '@MauticPluginWhatsAppEvolution/Integration/config_form.html.php';
     }
 }
